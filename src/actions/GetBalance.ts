6b8e81f2533f330486291007243af9ede6ada9ff
Anish <admin@anisharaz.com.np>
2024-04-12 10:15:26 +0530

    Fix: added unit type in getbalance

--- src/actions/GetBalance.ts
+++ src/actions/GetBalance.ts
@@ -1,14 +1,23 @@
-import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js";
+import {
+  Connection,
+  clusterApiUrl,
+  PublicKey,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
 import { SolNetwork } from "../types/All";
 
 export async function GetBalance(params: {
   PubKey: string;
   network: SolNetwork;
+  unit: "lamports" | "sol";
 }) {
   if (!PublicKey.isOnCurve(params.PubKey)) {
     throw new Error("Invalid Public Key");
   }
   const conn = new Connection(clusterApiUrl(params.network));
   const balance = await conn.getBalance(new PublicKey(params.PubKey));
+  if (params.unit === "sol") {
+    return balance / LAMPORTS_PER_SOL;
+  }
   return balance;
 }
