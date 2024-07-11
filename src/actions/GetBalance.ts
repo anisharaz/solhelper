2677fe39175a2012a8f588d924cdf56d5ccc7fe7
Anish Araz <anisharaz919@gmail.com>
2024-07-11 10:06:47 +0530

    UPDATE: network url in getbalance action

--- src/actions/GetBalance.ts
+++ src/actions/GetBalance.ts
@@ -4,17 +4,17 @@ import {
   PublicKey,
   LAMPORTS_PER_SOL,
 } from "@solana/web3.js";
-import { SolNetwork } from "../types/All";
 
 export async function GetBalance(params: {
   PubKey: string;
-  network: SolNetwork;
   unit: "lamports" | "sol";
 }) {
   if (!PublicKey.isOnCurve(params.PubKey)) {
     throw new Error("Invalid Public Key");
   }
-  const conn = new Connection(clusterApiUrl(params.network));
+  const conn = new Connection(
+    process.env.solana_rpc_url || clusterApiUrl("devnet")
+  );
   const balance = await conn.getBalance(new PublicKey(params.PubKey));
   if (params.unit === "sol") {
     return balance / LAMPORTS_PER_SOL;
