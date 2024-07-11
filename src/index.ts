592be2d4d47fdfc6b1a1878ea777f31b162f5f65
Anish Araz <anisharaz919@gmail.com>
2024-07-11 23:11:46 +0530

    FIX: emported from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { SendSolana } from "./actions/SendSol";
 export { GetBalance } from "./actions/GetBalance";
 export { SendToken } from "./actions/SendToken";
+export { SendTokenTransaction } from "./actions/SendTokenTransaction";
